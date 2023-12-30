eb5472ae11c6572816ae83cdd6b39d33845ccd41
lulueve3 <89736351+lulueve3@users.noreply.github.com>
2023-12-31 04:13:24 +0700

    update description in product screen

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -10,6 +10,8 @@ import Loader from '../components/Loader'
 import { addToCart, editCartItemQuantity } from '../action/cartAction'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { convertToHTML, convertFromHTML } from 'draft-convert';
+
 
 const ProductScreen = ({ }) => {
 
@@ -49,6 +51,10 @@ const ProductScreen = ({ }) => {
     useEffect(() => {
         dispatch(listProductDetail(id));
     }, [dispatch, id])
+
+    const renderHTML = (htmlString) => {
+        return { __html: htmlString };
+    };
 
     useEffect(() => {
         if (product && product.variants) {
@@ -93,151 +99,154 @@ const ProductScreen = ({ }) => {
             {
                 loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> :
                     product?.title ? (
-                        <Row>
-                            <Col md={5}>
-                                <div style={{ position: 'relative', display: 'inline-block' }}>
-                                    <Image
-                                        src={product.images[selectedImage]?.src || `${process.env.PUBLIC_URL}/imgNotFound.png`}
-                                        alt={product.title}
-                                        fluid
-                                        style={{ zIndex: 1 }} // Ensure that the main image is above the buttons
-                                    />
+                        <>
+                            <Row>
+                                <Col md={4}>
+                                    <div style={{ position: 'relative', display: 'inline-block' }}>
+                                        <Image
+                                            src={product.images[selectedImage]?.src || `${process.env.PUBLIC_URL}/imgNotFound.png`}
+                                            alt={product.title}
+                                            fluid
+                                            style={{ zIndex: 1, maxHeight: '300px' }} // Ensure that the main image is above the buttons
+                                        />
 
-                                    <Button
-                                        className="gallery-nav-button"
-                                        onClick={handlePrevClick}
-                                        style={{
-                                            position: 'absolute',
-                                            top: '50%',
-                                            left: '10px',
-                                            transform: 'translateY(-50%)',
-                                            background: 'rgba(128, 128, 128, 0.5)', // Set the background color to gray
-                                            color: 'white', // Set the text color to white
-                                        }}
-                                    >
-                                        &lt;
-                                    </Button>
+                                        <Button
+                                            className="gallery-nav-button"
+                                            onClick={handlePrevClick}
+                                            style={{
+                                                position: 'absolute',
+                                                top: '50%',
+                                                left: '10px',
+                                                transform: 'translateY(-50%)',
+                                                background: 'rgba(128, 128, 128, 0.5)', // Set the background color to gray
+                                                color: 'white', // Set the text color to white
+                                            }}
+                                        >
+                                            &lt;
+                                        </Button>
 
-                                    <Button
-                                        className="gallery-nav-button"
-                                        onClick={handleNextClick}
-                                        style={{
-                                            position: 'absolute',
-                                            top: '50%',
-                                            right: '10px',
-                                            transform: 'translateY(-50%)',
-                                            background: 'rgba(128, 128, 128, 0.5)', // Set the background color to gray
-                                            color: 'white', // Set the text color to white
-                                        }}
-                                    >
-                                        &gt;
-                                    </Button>
+                                        <Button
+                                            className="gallery-nav-button"
+                                            onClick={handleNextClick}
+                                            style={{
+                                                position: 'absolute',
+                                                top: '50%',
+                                                right: '10px',
+                                                transform: 'translateY(-50%)',
+                                                background: 'rgba(128, 128, 128, 0.5)', // Set the background color to gray
+                                                color: 'white', // Set the text color to white
+                                            }}
+                                        >
+                                            &gt;
+                                        </Button>
 
-                                    <div className="thumbnails-container">
-                                        {product.images.map((image, index) => (
-                                            <div key={index} className="d-inline-block me-2">
-                                                <Image
-                                                    src={image.src || `${process.env.PUBLIC_URL}/imgNotFound.png`}
-                                                    alt={`Thumbnail ${index + 1}`}
-                                                    fluid
-                                                    onClick={() => setSelectedImage(index)}
-                                                    className={`thumbnail ${index === selectedImage ? 'selected-thumbnail' : ''}`}
-                                                    style={{ width: '50px', height: '50px', margin: '5px' }}
-                                                />
-                                            </div>
-                                        ))}
+                                        <div className="thumbnails-container">
+                                            {product.images.map((image, index) => (
+                                                <div key={index} className="d-inline-block me-2">
+                                                    <Image
+                                                        src={image.src || `${process.env.PUBLIC_URL}/imgNotFound.png`}
+                                                        alt={`Thumbnail ${index + 1}`}
+                                                        fluid
+                                                        onClick={() => setSelectedImage(index)}
+                                                        className={`thumbnail ${index === selectedImage ? 'selected-thumbnail' : ''}`}
+                                                        style={{ width: '50px', height: '50px', margin: '5px' }}
+                                                    />
+                                                </div>
+                                            ))}
+                                        </div>
                                     </div>
-                                </div>
-                            </Col>
+                                </Col>
 
 
 
-                            <Col md={3}>
-                                <ListGroup variant='flush'>
-                                    <ListGroupItem>
-                                        <h3>{product.title}</h3>
-
-                                    </ListGroupItem>
-
-
-                                    <ListGroupItem>
-                                        Price: ${product.variants[0].price}
-                                    </ListGroupItem>
-                                    <ListGroupItem>
-                                        Description: {product.description}
-                                    </ListGroupItem>
-                                </ListGroup>
-                            </Col>
-                            <Col md={4}>
-                                <Card>
+                                <Col md={4}>
                                     <ListGroup variant='flush'>
-                                        <ListGroup.Item>
-                                            <Row>
-                                                <Col> Price:</Col>
-                                                <Col>
-                                                    <strong>${product.variants[selectedButton].price}</strong>
-                                                </Col>
-                                            </Row>
+                                        <ListGroupItem>
+                                            <h3>{product.title}</h3>
 
-                                        </ListGroup.Item>
-                                        <ListGroup.Item>
-                                            <Row>
-                                                <Col> Quantity:</Col>
-                                                <Col>
-                                                    <strong>{product.variants[selectedButton].quantity}</strong>
-                                                </Col>
-                                            </Row>
-                                            {product.variants.length < 2 ? null : (
-                                                <>
-
-                                                    {product.variants.map((variant, index) => (
-                                                        <Button
-                                                            key={index}
-                                                            variant={selectedButton === index ? 'info' : 'light'}
-                                                            type="button"
-                                                            onClick={() => handleButtonClick(index, variant.price)}
-                                                        >
-                                                            {variant.option1 + ' ' + variant.option2 + " " + variant.option3}
-                                                        </Button>
-                                                    ))}
-                                                </>
-                                            )}
-                                        </ListGroup.Item>
-                                        <ListGroup.Item>
-                                            {product.variants[selectedButton].quantity <= 0 ? (
-                                                <Button className='btn-block' type='button' disabled variant="danger">
-                                                    Out Of Stock
-                                                </Button>
-                                            ) : (
-                                                <>
-                                                    <div className="d-flex mb-2 align-items-center">
-                                                        <span className="mr-2">Enter Quantity:</span>
-                                                        <div className="input-group">
-                                                            <input
-                                                                type="number"
-                                                                value={qty}
-                                                                min={1}
-                                                                max={product.variants[selectedButton].quantity}
-                                                                onChange={(e) => setQty(Math.min(parseInt(e.target.value), product.variants[selectedButton].quantity))}
-                                                                className="form-control text-center"
-                                                                style={{ width: '100%' }}
-                                                            />
-                                                        </div>
-                                                    </div>
-                                                    <Button className='btn-block' type='button' onClick={addToCartHandler} variant="success">
-                                                        Add To Cart
-                                                    </Button>
-                                                </>
-                                            )}
-                                        </ListGroup.Item>
+                                        </ListGroupItem>
 
 
-
-
+                                        <ListGroupItem>
+                                            Price: ${product.variants[0].price}
+                                        </ListGroupItem>
                                     </ListGroup>
-                                </Card>
-                            </Col>
-                        </Row>
+                                </Col>
+                                <Col md={4}>
+                                    <Card>
+                                        <ListGroup variant='flush'>
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col> Price:</Col>
+                                                    <Col>
+                                                        <strong>${product.variants[selectedButton].price}</strong>
+                                                    </Col>
+                                                </Row>
+
+                                            </ListGroup.Item>
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col> Quantity:</Col>
+                                                    <Col>
+                                                        <strong>{product.variants[selectedButton].quantity}</strong>
+                                                    </Col>
+                                                </Row>
+                                                {product.variants.length < 2 ? null : (
+                                                    <>
+
+                                                        {product.variants.map((variant, index) => (
+                                                            <Button
+                                                                key={index}
+                                                                variant={selectedButton === index ? 'info' : 'light'}
+                                                                type="button"
+                                                                onClick={() => handleButtonClick(index, variant.price)}
+                                                            >
+                                                                {variant.option1 + ' ' + variant.option2 + " " + variant.option3}
+                                                            </Button>
+                                                        ))}
+                                                    </>
+                                                )}
+                                            </ListGroup.Item>
+                                            <ListGroup.Item>
+                                                {product.variants[selectedButton].quantity <= 0 ? (
+                                                    <Button className='btn-block' type='button' disabled variant="danger">
+                                                        Out Of Stock
+                                                    </Button>
+                                                ) : (
+                                                    <>
+                                                        <div className="d-flex mb-2 align-items-center">
+                                                            <span className="mr-2">Enter Quantity:</span>
+                                                            <div className="input-group">
+                                                                <input
+                                                                    type="number"
+                                                                    value={qty}
+                                                                    min={1}
+                                                                    max={product.variants[selectedButton].quantity}
+                                                                    onChange={(e) => setQty(Math.min(parseInt(e.target.value), product.variants[selectedButton].quantity))}
+                                                                    className="form-control text-center"
+                                                                    style={{ width: '100%' }}
+                                                                />
+                                                            </div>
+                                                        </div>
+                                                        <Button className='btn-block' type='button' onClick={addToCartHandler} variant="success">
+                                                            Add To Cart
+                                                        </Button>
+                                                    </>
+                                                )}
+                                            </ListGroup.Item>
+                                        </ListGroup>
+                                    </Card>
+                                </Col>
+                            </Row>
+                            <div style={{ marginTop: '50px' }}>
+                                <h3>Description</h3>
+                                <div dangerouslySetInnerHTML={renderHTML(product.description)} />
+                            </div>
+
+
+                        </>
+
+
                     ) : (
                         <Message variant='info'>No product available</Message>
                     )
